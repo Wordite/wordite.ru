7f2f14e9395bf9653f2c94c1327f7d7025d21b73
Wordite <wordite123@gmail.com>
2025-06-03 15:28:31 +0300

    add awards

--- src/widgets/awards/Awards.jsx
+++ src/widgets/awards/Awards.jsx
@@ -1,5 +1,7 @@
 import style from './Awards.module.scss'
 import web2023 from '../../assets/img/web_2023.png'
+import web2024 from '../../assets/img/web_2024.png'
+import web2024Photo from '../../assets/img/web_2024_photo.png'
 import itPlanet from '../../assets/img/it_planet.png'
 import { useAnimations } from '../../app/hooks/useAnimations'
 import { useStore } from '@nanostores/react'
@@ -26,6 +28,8 @@ const Awards = () => {
 
   return (
     <section className={style.awards}>
+      <img className={style.award} src={web2024.src} alt='web 2024' />
+      <img className={style.award} src={web2024Photo.src} alt='web 2024 photo' />
       <img className={style.award} src={web2023.src} alt='web 2023' />
       <img className={style.award} src={itPlanet.src} alt='it planet' />
     </section>
